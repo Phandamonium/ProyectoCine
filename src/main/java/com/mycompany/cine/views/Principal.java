b45d30023e47e9507d54663ec54a3cc8c86ffcc6
mlarr <mlarr@Monz>
2023-10-27 22:48:24 -0300

    Carga inicial

---
+++ src/main/java/com/mycompany/cine/views/Principal.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cine.views;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mlarr
+ */
+public class Principal extends JFrame implements ActionListener{
+
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
